45a3c1a9ca1b448923cda9ca263e93c08324c3f5
Manish Agarwal <73208573+Manish06097@users.noreply.github.com>
2022-08-17 11:39:25 +0530

    Create Countingsort.h

---
+++ Search-Sort/Countingsort.h
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int getMax(int a[], int n) {  
+   int max = a[0];  
+   for(int i = 1; i<n; i++) {  
+      if(a[i] > max)  
+         max = a[i];  
+   }  
+   return max;   
+}  
+
+void counting_sort(int a[],int n)
+{
+    int output[n+1];  
+   int max = getMax(a, n);  
+   int count[max+1]; 
+  
+  
+  for (int i = 0; i <= max; ++i)   
+  {  
+    count[i] = 0; 
+  }  
+    
+  for (int i = 0; i < n; i++)
+  {  
+    count[a[i]]++;  
+  }  
+  
+   for(int i = 1; i<=max; i++)   
+      count[i] += count[i-1];  
+  for (int i = n - 1; i >= 0; i--) {  
+    output[count[a[i]] - 1] = a[i];  
+    count[a[i]]--;
+}  
+  
+   for(int i = 0; i<n; i++) {  
+      a[i] = output[i]; 
+   }  
+}
